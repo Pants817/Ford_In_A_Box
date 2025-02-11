7fd949c3f555d09dca410ee44a6270b625d28c44
Toby Heath <toby817@outlook.com>
2025-02-11 13:08:02 +0000

    Full control of the IPC and logs from bcm and ecu

--- BCM_Test.py
+++ BCM_Test.py
@@ -2,8 +2,10 @@ from utils.canbus import CanBus
 
 with CanBus(channel = "can0", log_folder = r"log_files") as can0:
     try:
+        can0.send_periodic(data = [0x40, 0x00,0x00, 0x80, 0x00,0x00,0x00,0x00],period = 0.5, arb_id = 0x3b3, )
         while True:
             response = can0.recv()
             print(response)
     except KeyboardInterrupt:
         pass
+print("stopped")
---
+++ PCM_Test.py
@@ -0,0 +1,20 @@
+from utils.canbus import CanBus
+import time
+
+with CanBus(channel = "can0", log_folder = r"log_files/pcm") as can0:
+    can_id = 0x580
+    counter = 0x00
+    ign_id = 0x3b2
+    try:
+        while True:
+            response = can0.recv()
+            print(response)
+            ignition_data = [0x40, 0x00,0x00, 0x80, 0x00,0x00,0x00,0x00]
+            sent = can0.send(data = ignition_data, arb_id = 0x3b2)
+            #wake_up = [counter, 0x00, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff]
+            #sent = can0.send(data = wake_up, arb_id = can_id+counter)
+            #print(hex(can_id+counter))
+            #counter+=1
+            #time.sleep(0.5)
+    except KeyboardInterrupt:
+        pass
