1843e051ba9a34499aecdd37a0ba977885d06856
Toby Heath <toby817@outlook.com>
2025-04-24 13:37:34 +0100

    added new log files

--- full_system.py
+++ full_system.py
@@ -4,7 +4,7 @@ import time
 with CanBus(channel = "can0", log_folder = r"log_files/full_system") as can0:
     try:
         while True:
-            pass
+            sent = can0.send(data = [0,0,0,0,0,0,0,0], arb_id = 0)
     except KeyboardInterrupt:
         pass
 print("stopped")
